decf638d1f077fe1bdb0df8d4d1116cbb8590dfe
Vmiguel96 <103062540+Vmiguel96@users.noreply.github.com>
2022-10-05 17:58:46 +0200

    Add files via upload

---
+++ Repo/07-06/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace _07_06
+{
+    class Program
+    {
+        public static void ApropiadaCadena(ref string cadena)
+        {
+
+            //1-Pasar Cadena a minuscula(ToLower)
+            cadena = cadena.ToLower();
+            //2-Primera posicion en mayuscula (ToUpper)
+            cadena = Convert.ToString(cadena[0]).ToUpper() + cadena.Substring(1);
+            //cadena = cadena.Substring(0,1).ToUpper()+cadena.Substring(1);
+            //cadena = cadena.ToUpper();
+            //Localizar todos los '.' con IndexOf con while
+            //Ir haciendo subString para buscar los siguientes "."
+            // Cambiar a mayuscula la siguiente letra despues del punto con Replace
+            //while (cadena.IndexOf(".",indice)>=0) 
+            //{
+            //    cadenaTemporal = cadena.Substring(indice, cadena.IndexOf(".", indice));
+            //    indice = cadena.IndexOf(".",indice);
+
+            //}
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i] == '.')
+                {
+                    cadena = cadena.Substring(0, i) + cadena.Substring(i + 1, 1).ToUpper() + cadena.Substring(i + 2);
+
+                }
+
+            }
+
+        }
+
+        public static int SumaCifras(int numero)
+        {
+            int resultado=0;
+
+            while (numero>0)
+            {
+                resultado += numero % 10;
+                numero /= 10; 
+                
+            }
+            return resultado;
+        }
+
+        public static string EscribirAlRever(string cadena) 
+        {
+            for (int i=cadena.Length-1;i>=0;i--) 
+            {
+                Console.Write(cadena[i]);
+            }
+            return cadena;
+        }
+
+        public static void CuantasVocalesDeCada(string cadena, out int a, out int e, out int i, out int o, out int u) 
+        {
+             a = 0;
+             e = 0;
+             i = 0;
+             o = 0;
+             u = 0;
+
+            for (int j=0;j<cadena.Length;j++) 
+            {
+                switch (cadena[j])
+                {
+                    case 'a':
+                        a++;
+                        break;
+                    case 'e':
+                        e++;
+                        break;
+                    case 'i':
+                        i++;
+                        break;
+                    case 'o':
+                        o++;
+                        break;
+                    case 'u':
+                        u++;
+                        break;
+                }
+            }
+        }
+
+        public static double Media(string notas) 
+        {
+            double media = 0;
+            int cantidadErrores=0;
+
+            string[] auxiliar = notas.Split(';');
+            for (int i=0;i<auxiliar.Length;i++)
+            {
+                 double temp=0;
+                media += Convert.ToDouble(auxiliar[i]);
+
+
+                //solucion pro
+                if (Double.TryParse(auxiliar[i],out temp)) 
+                {
+                media = media + temp;
+            }
+                else
+            {
+                cantidadErrores++;
+                Console.WriteLine($"");
+            }
+        }
+            media = media / auxiliar.GetLength(0);
+
+            return media;
+        }
+
+
+        public static string[] ArrayCadena(string[] palabras) 
+        {
+            Array.Sort(palabras);
+            Array.Reverse(palabras);
+            return palabras;
+        }
+
+        public static int ArrayNumeros(int[] numeros,int buscar) 
+        {
+
+            int [] numeroValor = Array.FindAll(numeros,n=> n == buscar);
+            return numeroValor.Length;
+        }
+
+        public static string ConvertirFecha(DateTime fecha) 
+        {
+            string[] dias = { "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
+            string diaHoy = dias[(int)fecha.DayOfWeek];
+            return $"Hoy es {diaHoy}, {fecha.Day:D2}/{fecha.Month:D2}/{fecha.Year}";
+        }
+
+        public static DateTime ConsultaFecha(string fecha)
+        {
+            string[] partes = fecha.Split("-");
+            int dia = Convert.ToInt32(partes[0]);
+            int mes = Convert.ToInt32(partes[1]);
+            int anyo = Convert.ToInt32(partes[2]);
+            DateTime fechanueva = new DateTime(anyo,mes,dia);
+            DateTime fechaFutura = fechanueva.AddYears(2).AddMonths(3).AddDays(3);
+            return fechaFutura;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            //int a=0, e=0, i=0, o=0, u=0;
+            //string cadena = "esta es .la prueba.n ";
+            //ApropiadaCadena(ref cadena);
+            //Console.WriteLine(cadena);
+            //Console.WriteLine(SumaCifras(654));
+            //EscribirAlRever("Hola");
+
+            //CuantasVocalesDeCada("aeiideioeodeaw", out a, out e, out i, out o, out u);
+            //Console.WriteLine($"\nA:{a} E:{e} I:{i} O:{o} U:{u}");
+
+            string numeros = "5;5;5;5;5;5,5891";
+            Console.WriteLine(Media(numeros));
+
+            string[] ListaPalabras = {"Boligrafo","Mando","TV","Monitor","Raton","Teclado","Xbox","Libreta" };
+            Console.WriteLine($"{String.Join(", ", ArrayCadena(ListaPalabras))}");
+
+            int[] listaNumeros = {1,3,2,4,5,1,2,6,9 };
+
+            Console.WriteLine($"Las veces que aparece el numero {String.Join("*", ArrayNumeros(listaNumeros,4))}");
+            Console.WriteLine(DateTime.Now);
+
+            //DateTime
+
+            DateTime fechaHoy = new DateTime(2021, 06,07,19,59,59);
+            Console.WriteLine(fechaHoy);
+            DateTime fechaAhora = DateTime.Today;
+            Console.WriteLine(fechaAhora);
+
+            Console.WriteLine(ConvertirFecha(DateTime.Now));
+            Console.WriteLine(ConsultaFecha("06-10-2021"));
+
+
+
+        }
+    }
+}
---
+++ Repo/08-06/Program.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Threading;
+
+namespace _08_06
+{
+    class Program
+    {
+        public static bool Salir(string palabra, string palabra2)
+        {
+            bool resultado;
+            if ((palabra == "me rindo") || (palabra2=="me rindo") ) 
+            {
+                resultado = true;
+            }
+            else 
+            {
+                resultado = false;
+            }
+            return resultado;
+            //return palabra == "me rindo";
+        }
+
+        public static void PalabrasEncadenadas() 
+        {
+            string palabra = "";
+            string palabra2 = "";
+
+            Console.Write("Introduzca palabra inicial jugador 1: ");
+            palabra = Console.ReadLine();
+
+
+            while (!Salir(palabra,palabra2) ) 
+            {
+                Console.Write("Introduzca palabra jugador 2: ");
+                palabra = Console.ReadLine();
+                if (EsCorrecto(palabra, palabra2))
+                {
+                    Console.WriteLine("Es Correcto");
+                }
+                else
+                {
+                    Console.WriteLine("Es Incorrecto");
+                }
+                Console.Write("Introduzca palabra jugador 1: ");
+                palabra2 = Console.ReadLine();
+                if (EsCorrecto(palabra2, palabra))
+                {
+                    Console.WriteLine("Es Correcto");
+                }
+                else
+                {
+                    Console.WriteLine("Es Incorrecto");
+                }
+
+            }
+
+        }
+
+        public static bool EsCorrecto(string palabra, string palabra2)
+        {
+
+            palabra=palabra.ToLower();
+            palabra2=palabra2.ToLower();
+
+            //if (String.Compare(Convert.ToString(palabra[palabra.Length - 1]), Convert.ToString(palabra2[0])) == 0)
+            //{
+            //    resultado = true;
+            //}
+            //else 
+            //{
+            //    resultado = false;
+            //}
+
+            
+            return palabra[palabra.Length-1]==palabra[0];
+        }
+        public static void MaxMin(int[] ejemplo,ref int max, ref int min) 
+        {
+            Array.Sort(ejemplo);
+            max = ejemplo[ejemplo.Length - 1];
+            min = ejemplo[0];
+        }
+
+        public static void EscribeTexto(string cadena) 
+        {
+            cadena = cadena.ToLower();
+            string aux = "";
+            for (int i=0;i<cadena.Length;i++) 
+            {
+                if (Char.IsLetterOrDigit(cadena[i]))
+                {
+
+                }
+                else 
+                {
+                    aux = cadena[i+1].ToString();
+                    cadena = cadena.Remove(i+1,1);
+
+                    cadena = cadena.Insert(i+1,aux.ToUpper());
+                    cadena = cadena.Remove(i, 1);
+                    i--;
+                    //Console.WriteLine($"{aux}-{i}-{cadena}");
+                    
+                }
+               
+            }
+            Console.WriteLine($"La cadena final:{cadena}");
+
+        }
+
+        public static bool NumeroHashad(int numero) 
+        {
+            string numeroLetra = numero.ToString();
+            int suma = 0;
+            for (int i=0;i<numeroLetra.Length;i++) 
+            {
+                suma += Convert.ToInt32(numeroLetra[i]);
+            }
+            if (numero % suma == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void ContarParesImpares(int numero, out int pares, out int impares) 
+        {
+            pares = 0;
+            impares = 0;
+
+            string numeroLetra = numero.ToString();
+
+            for (int i = 0; i < numeroLetra.Length; i++)
+            {
+                if (Convert.ToInt32(numeroLetra[i]) % 2 == 0)
+                {
+                    pares++;
+                }
+                else 
+                {
+                    impares++;
+                }
+            }
+        }
+
+        public static void Reloj() 
+        {
+            while (true) 
+            {
+                Console.Clear();
+                Console.Write($"{DateTime.Now.Hour}/");
+                Console.Write($"{DateTime.Now.Minute}/");
+                Console.Write($"{DateTime.Now.Second}/");
+                Console.Write(DateTime.Now.Millisecond);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void DiaCumple() 
+        {
+            string fecha = "";
+            string[] dias = { "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
+            Console.WriteLine("Introduce la fecha de tu cumpleaños DD/MM/AAAA");
+            fecha = Console.ReadLine();
+            DateTime parsedDate = new DateTime();
+
+            if (DateTime.TryParse(fecha, out parsedDate))
+            {
+                //Metodo1
+                string diaHoy = dias[(int)parsedDate.DayOfWeek];
+                Console.WriteLine($"Naciste el {fecha} un {diaHoy}");
+                //Metodo 2 Sin DayOfWeek
+                int posicion = 0;
+                string fechaLarga = parsedDate.ToLongDateString();
+                posicion = fechaLarga.IndexOf(',');
+                string diaSemana = fechaLarga.Substring(0,posicion);
+                Console.WriteLine(diaSemana);
+                //Metodo 2 Super Pro
+                DateTime fechaModificada = new DateTime(DateTime.Now.Year,parsedDate.Month,parsedDate.Day);
+                if(DateTime.Now > fechaModificada)
+                    fechaModificada = new DateTime(DateTime.Now.Year, parsedDate.Month, parsedDate.Day);
+                Console.WriteLine($"Naciste el {fecha} un {fechaLarga.Substring(0,parsedDate.ToLongDateString().IndexOf(','))} ");
+                var horas = (DateTime.Now - fechaModificada).ToString(@"dd\d\ hh\h\ mm\m\ ");
+                Console.WriteLine($"{horas}");
+            }
+            else 
+            {
+                Console.WriteLine($"Error al convertir fecha: {fecha}");
+            }
+
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("es-ES");
+            int max=0, min=0;
+            int[] ejemplo = {20,10,5,2 };
+            MaxMin(ejemplo, ref max, ref min);
+            Console.WriteLine($"El maximo es: {max} y el minimo es {min}");
+            EscribeTexto("hola.que tal,,estAs");
+            if (NumeroHashad(152))
+            {
+                Console.WriteLine("Si es un numero Harshad 152");
+            }
+            else 
+            {
+                Console.WriteLine("No es un numero Harshad");
+            }
+
+
+
+            int a =123456;
+            int b = 0;
+            int c = 0;
+            ContarParesImpares(a, out b, out c);
+            Console.WriteLine($"{a} {b} {c}");
+            //Reloj();
+            DiaCumple();
+            
+
+            //bool salir = false;
+            //string menu = "";
+            //Console.WriteLine("Bienvenido al Juego");
+            //do 
+            //{
+            //    Console.WriteLine("1.- Palabras Encadenadas");
+            //    Console.WriteLine("2.- Adivina el numero");
+            //    Console.WriteLine("3.- Salir del Programa");
+
+            //    menu = Console.ReadLine();
+
+            //    switch (menu) 
+            //    {
+            //        case "1":
+            //            PalabrasEncadenadas();
+            //            break;
+            //        case "2":
+
+            //            break;
+            //        case "3":
+            //            salir = true;
+            //            break;
+            //    }
+                
+            //}while (!salir) ;
+        }
+    }
+}
