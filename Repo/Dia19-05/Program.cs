a58b654dc29cd2d40a71946b842fad744e0febed
Vmiguel96 <103062540+Vmiguel96@users.noreply.github.com>
2022-10-05 18:05:53 +0200

    Add files via upload

---
+++ Repo/Dia19-05/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+namespace Dia19_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero, numero2;
+            //Console.WriteLine("Introduce un Numero: ");
+            //numero = Convert.ToInt32(Console.ReadLine());
+            //numero2 = 1; 
+            //Console.WriteLine($"{numero} x 1 = {numero* numero2}");
+            //numero2++;
+            //Console.WriteLine($"{numero} x {numero2} = {numero* numero2}");
+            //numero2++;
+            //Console.WriteLine($"{numero} x 1 = {numero* numero2}");
+            //numero2++;
+            //Console.WriteLine(numero* numero2);
+            //numero2++;
+            //Console.WriteLine(numero* numero2);
+            //numero2++;
+            //Console.WriteLine(numero* numero2);
+            //numero2++;
+            //Console.WriteLine(numero * numero2);
+            //numero2++;
+            //Console.WriteLine(numero * numero2);
+            //numero2++;
+            //Console.WriteLine(numero * numero2);
+            //numero2++;
+            //Console.WriteLine(numero * numero2);
+
+
+            //int segundosUsuario, segundos, minutos , horas;
+            //Console.WriteLine("Introduce Un Numero de Segundos: ");
+            //segundosUsuario = Convert.ToInt32(Console.ReadLine());
+
+
+            //minutos = segundosUsuario / 60;
+            //segundos = segundosUsuario % 60;
+            //horas = minutos / 60;
+
+            //Console.WriteLine($"{segundosUsuario} segundos son: {horas}horas {minutos}minutos {segundos}segundos");
+
+            //int dato, dato2;
+            //Console.WriteLine("Introduce Primer Numero");
+            //dato = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Introduce Segundo Numero");
+            //dato2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"La Suma de {dato} + {dato2} es: {dato + dato2}");
+            //Console.WriteLine($"La Resta de {dato} - {dato2} es: {dato - dato2}");
+            //Console.WriteLine($"Multiplicación :{dato * dato2}");
+            //Console.WriteLine(dato / dato2);
+
+            //int metros;
+            //double millas;
+            //Console.WriteLine("Introduce Numero de Metros");
+            //metros = Convert.ToInt32(Console.ReadLine());
+            //millas = (double)metros / 1609;
+            //Console.WriteLine($"{metros}metros son {millas} millas");
+
+            //string nombre;
+            //int edad;
+            //Console.WriteLine("Introduce Nombre");
+            //nombre = Console.ReadLine();
+            //Console.WriteLine("Introduce la Edad");
+            //edad = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{nombre} tiene {edad} años");
+
+            //int botellas;
+            //int precio = 25;
+            //int precioTotal;
+            //int euros, centimos;
+
+
+            //Console.WriteLine("Intoduce el Numero de Botellas");
+            //botellas = Convert.ToInt32(Console.ReadLine());
+
+            //precioTotal = precio * botellas;
+
+            //euros = precioTotal / 100;
+            //centimos = precioTotal % 100;
+
+            //Console.WriteLine($"Las {botellas} tiene un precio de:{euros}euros,{centimos}centimos");
+
+            //int horaEntrada;
+            //int horaSalida;
+            //int horaE;
+            //int horaS;
+            //int precioEuros;
+            //int precioCentimos;
+
+            //Console.WriteLine("Introduce Hora Entrada");
+            //horaEntrada = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Introduce Hora Salida");
+            //horaSalida = Convert.ToInt32(Console.ReadLine());
+            //horaE = horaEntrada / 100;
+            //horaS = horaSalida / 100;
+            //precioEuros = (horaS - horaE) * 60/100;
+
+            //Console.WriteLine($"Precio a pagar: {precioEuros}");
+
+
+            //double gradosC , gradosF;
+
+            //Console.WriteLine("Introduce Grados C");
+            //gradosC = Convert.ToDouble(Console.ReadLine());
+
+            //gradosF = gradosC * 33.8;
+
+            //Console.WriteLine($"Los {gradosC} Grados Celsius son:{gradosF} Grados Fahrenheit");
+
+            //string nombreUsuario;
+            //Console.WriteLine("Introduce Nombre");
+            //nombreUsuario = Console.ReadLine();
+            //Console.WriteLine($"Buenos Dias {nombreUsuario}");
+
+            //int numeroEntero;
+            //Console.Write("Introduce Numero: ");
+            //numeroEntero = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"El doble de {numeroEntero} es {numeroEntero * 2}");
+            //Console.WriteLine($"El triple de {numeroEntero} es {numeroEntero * 3}");
+            //Console.WriteLine($"El cuadrado de {numeroEntero} es {Math.Pow(numeroEntero,2)}");
+            //Console.WriteLine($"El cubo de {numeroEntero} es {Math.Pow(numeroEntero, 3)}");
+            //Console.WriteLine($"La raiz cuadrada de {numeroEntero} es {Math.Sqrt(numeroEntero)}");
+
+            //int gradosCentigrados;
+            //double gradosFahrenheit;
+
+            //Console.Write("Introduzca Grados centigrados");
+            //gradosCentigrados = Convert.ToInt32(Console.ReadLine());
+            //gradosFahrenheit = 32 +(9 * gradosCentigrados / 5);
+            //Console.WriteLine($"Los {gradosCentigrados} grados centigrados son: {gradosFahrenheit:F2} Grados Fanhrenheit");
+
+            //int numeroEntero, unidades, decenas, centenas, auxiliar;
+
+            //Console.Write("Intoduce un Numero de 3 Cifras");
+            //numeroEntero = Convert.ToInt32(Console.ReadLine());
+            //auxiliar = numeroEntero / 10;
+            //unidades = numeroEntero % 10;
+            //centenas = auxiliar / 10;
+            //decenas = auxiliar % 10;
+            //Console.WriteLine($"Centenas{centenas}");
+            //Console.WriteLine($"Decenas{decenas}");
+            //Console.WriteLine($"Unidades{unidades}");
+
+
+            //int dia, mes, anyo, sumaFecha;
+            //int miles, centenas, decenas, unidades,auxiliar;
+            //Console.Write("Intoduce tu Dia de Nacimiento");
+            //dia = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Intoduce tu Mes de Nacimiento");
+            //mes = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Intoduce tu Año de Nacimiento");
+            //anyo = Convert.ToInt32(Console.ReadLine());
+
+            //sumaFecha = dia + mes + anyo;
+            //unidades = sumaFecha % 10;
+            //auxiliar = sumaFecha / 10;
+
+            //decenas = auxiliar % 10;
+            //auxiliar = auxiliar / 10;
+
+            //centenas = auxiliar % 10;
+            //miles = auxiliar / 10;
+            //Console.WriteLine($"{sumaFecha}: -Valores {miles},{centenas},{decenas},{unidades}");
+            //Console.WriteLine($"Tu numero de la suerte es:{miles+centenas+decenas+unidades}");
+
+            string producto1,producto2,producto3;
+            double precio1,precio2,precio3;
+
+            Console.Write("Nombre del Primer Producto");
+            producto1 = Console.ReadLine();
+            Console.Write("Precio del Primer Producto");
+            precio1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nombre del Segundo Producto");
+            producto2 = Console.ReadLine();
+            Console.Write("Precio del Segundo Producto");
+            precio2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nombre del Tercer Producto");
+            producto3 = Console.ReadLine();
+            Console.Write("Precio del Tercer Producto");
+            precio3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{"NOMBRE",-15}{"PRECIO",12}{"CON IVA",12}");
+            Console.WriteLine($"{producto1,-15}{precio1,12:F2}€{precio1* 1.21,12:F2}");
+            Console.WriteLine($"{producto2,-15}{precio2,12:F2}€{precio2* 1.21,12:F2}");
+            Console.WriteLine($"{producto3,-15}{precio3,12:F2}€{precio3* 1.21,12:F2}");
+        }
+    }
+}
---
+++ Repo/Dia20-05/Program.cs
@@ -0,0 +1,363 @@
+﻿using System;
+
+namespace Dia20_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string nombre;
+            //int edad;
+            //Console.Write("Escribe tu nombre: ");
+            //nombre = Console.ReadLine();
+            //Console.Write("Escribe tu edad: ");
+            //edad = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Estos son tus datos \n\t nombre: {nombre}  \n\t edad: {edad} ");
+
+            //if (nombre == "Mari Chelo")
+            //{
+            //    Console.WriteLine("Hola Mari Chelo");
+            //}
+            //    Console.WriteLine("Vamos a trabajar");
+
+            //  1metro = 1609metros
+
+            //int millas, metros, kilometros;
+            //string conversion;
+
+            //Console.WriteLine("Introduce un numero de millas");
+            //millas = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Si quieres convertir a Kilometros escribe 1 o 2 para convertir a metros");
+            //conversion = Console.ReadLine();
+
+            //metros = millas * 1609;
+
+            //if (conversion == "1")
+            //{
+            //    kilometros = metros / 1000;
+            //    Console.WriteLine($"Estas son las {millas} millas convertidas en {kilometros} kilometros ");
+            //}
+            //else if (conversion == "2")
+            //{
+            //    Console.WriteLine($"Estas son las {millas} millas convertidas en {metros} metros ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Opción invalida");
+            //}
+
+
+
+            //int signo, numero;
+
+            //Console.WriteLine($"Escribe un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //if (numero > 0)
+            //{
+            //    signo = +1;
+            //    Console.WriteLine($"El signo es {signo}");
+            //}
+
+            //else if (numero < 0) 
+            //{
+            //    signo = -1;
+            //    Console.WriteLine($"El signo es {signo}");
+            //}
+
+            //else 
+            //{
+            //    signo = 0;
+            //    Console.WriteLine($"El signo es {signo}");
+            //}
+
+            //int numeroMultiplo,multiplo; //1 si es de 2 y 3 //0 no si no lo es 
+
+            //Console.WriteLine($"Esctbe un numero");
+            //numeroMultiplo = Convert.ToInt32(Console.ReadLine());
+
+            //if (numeroMultiplo%2==0 && numeroMultiplo%3==0)
+            //{
+            //    multiplo = 1;
+            //}
+            //else
+            //{
+            //    multiplo = 0;
+            //}
+            //if (multiplo == 1)
+            //{
+            //    Console.WriteLine($"El número es múltiplo de 2 y de 3");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"El número no es múltiplo de 2 y de 3");
+            //}
+            //Console.WriteLine($"Multiplo: {multiplo}");
+
+            //int numeroProducto, numeroProducto2,resultado;
+            //Console.WriteLine("Escribe un numero");
+            //numeroProducto = Convert.ToInt32(Console.ReadLine());
+
+            //if (numeroProducto != 0)
+            //{
+            //    Console.WriteLine("Escribe otro numero");
+            //    numeroProducto2 = Convert.ToInt32(Console.ReadLine());
+            //    resultado = numeroProducto * numeroProducto2;
+            //    Console.WriteLine($"La multiplicacion de {numeroProducto} y {numeroProducto2} es {resultado}");
+            //    if (resultado % 2 == 0)
+            //    {
+            //        Console.WriteLine($"El numero es par");
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine($"El numero es impar");
+            //    }
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"El producto es 0");
+            //}
+
+            //int numeroMes;
+
+            //Console.WriteLine($"Escribe un numero");
+            //numeroMes = Convert.ToInt32(Console.ReadLine());
+
+            //if (numeroMes > 0 && numeroMes<13)
+            //{
+            //    if (numeroMes == 1)
+
+            //    {
+            //        Console.WriteLine($"Enero");
+
+            //    }
+            //    else if (numeroMes == 2)
+
+            //    {
+            //        Console.WriteLine($"Febrero");
+
+            //    }
+            //    else if (numeroMes == 3)
+
+            //    {
+            //        Console.WriteLine($"Marzo");
+
+            //    }
+            //    else if (numeroMes == 4)
+
+            //    {
+            //        Console.WriteLine($"Abril");
+
+            //    }
+            //    else if (numeroMes == 5)
+
+            //    {
+            //        Console.WriteLine($"Mayo");
+
+            //    }
+            //    else if (numeroMes == 6)
+
+            //    {
+            //        Console.WriteLine($"Junio");
+
+            //    }
+            //    else if (numeroMes == 7)
+
+            //    {
+            //        Console.WriteLine($"Julio");
+
+            //    }
+            //    else if (numeroMes == 8)
+
+            //    {
+            //        Console.WriteLine($"Agosto");
+
+            //    }
+            //    else if (numeroMes == 9)
+
+            //    {
+            //        Console.WriteLine($"Septiembre");
+
+            //    }
+            //    else if (numeroMes == 10)
+
+            //    {
+            //        Console.WriteLine($"Octubre");
+
+            //    }
+            //    else if (numeroMes == 11)
+
+            //    {
+            //        Console.WriteLine($"Noviembre");
+
+            //    }
+            //    else if (numeroMes == 12)
+
+            //    {
+            //        Console.WriteLine($"Diciembre");
+
+            //    }
+
+            //    Console.WriteLine($"Numero incorrecto");
+            //}
+
+
+            //int numeroAño;
+
+            //Console.WriteLine("Escribe un año");
+            //numeroAño = Convert.ToInt32(Console.ReadLine());
+
+            //if (numeroAño%4==0 && (numeroAño % 100 == 0) && numeroAño % 400 == 0)
+            //{
+            //    Console.WriteLine("El año es Bisiesto");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("El año no es Bisiesto");
+            //}
+
+            //int numeroPares = 0;
+            //Console.WriteLine($"Introduce un Numero");
+            //numeroPares = Convert.ToInt32(Console.ReadLine());
+            //if (numeroPares%2 == 0 ) 
+            //{
+            //    Console.WriteLine($"El numero {numeroPares} es par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"El numero {numeroPares} es impar");
+            //}
+
+            //int numeroMultiplo10 = 0;
+            //Console.WriteLine($"Introduce un Numero");
+            //numeroMultiplo10 = Convert.ToInt32(Console.ReadLine());
+
+            //if (numeroMultiplo10 == 0)
+            //{
+            //    Console.WriteLine($"{numeroMultiplo10} no es multiplo");
+            //}
+
+            //else if (numeroMultiplo10 % 10 == 0)
+
+            //{
+            //    Console.WriteLine($"{numeroMultiplo10} es multiplo de 10");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{numeroMultiplo10} no es multiplo de 10");
+
+            //}
+
+            //int numero2Multiplo10 = 0;
+            //Console.WriteLine($"Introduce un Numero");
+            //numero2Multiplo10 = Convert.ToInt32(Console.ReadLine());
+
+            //if (numero2Multiplo10%10 ==0 && !(numero2Multiplo10 == 0 ))
+            //{
+            //    Console.WriteLine($"{numero2Multiplo10} es multiplo de 10");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"{numero2Multiplo10} no es multiplo de 10");
+
+            //char valor;
+            //Console.Write("Escribe una letra: ");
+            //valor = Console.ReadKey().KeyChar;
+
+            //if ((valor >= 'A' && valor <= 'Z') || valor == 'Ñ' )
+
+            //{
+            //    Console.WriteLine($"\nEs una letra mayúscula");
+            //}
+            //else
+
+            //{
+            //    Console.WriteLine($"\nEs una letra minuscula");
+            //}
+
+
+            //string cadena1,cadena2;
+
+            //Console.WriteLine("Escribe una texto");
+            //cadena1 = Console.ReadLine();
+
+            //Console.WriteLine("Escribe otro texto");
+            //cadena2 = Console.ReadLine();
+
+            //if (cadena1 == cadena2) 
+
+            //{
+            //    Console.WriteLine($" \n{cadena1} \n{cadena2} \n Las dos cadenas son iguales");
+            //}
+
+            //else 
+            //{
+            //    Console.WriteLine($" \n{cadena1} \n{cadena2} \nLas dos cadenas no son iguales");
+            //}
+
+            //double primerNumero, divisor;
+
+            //Console.WriteLine("Escribe un dividendo");
+            //primerNumero = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Escribe un divisor");
+            //divisor = Convert.ToDouble(Console.ReadLine());
+
+            //if (divisor == 0)
+            //{
+            //    Console.WriteLine($"No puedes dividir por {divisor}");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"La divison de : {primerNumero} y {divisor} es: {primerNumero /divisor}"); 
+            //}
+
+            int Numero1, Numero2, Numero3;
+
+            Console.WriteLine("Escribe el primer numero");
+            Numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe el segundo numero");
+            Numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe el tercer numero");
+            Numero3 = Convert.ToInt32(Console.ReadLine());
+
+            if (Numero1 > Numero2 && Numero1 > Numero3)       
+            {
+                Console.WriteLine($"El numero mayor es {Numero1}");
+            }
+            else if  (Numero2 > Numero1 && Numero2 > Numero3) 
+            {
+                Console.WriteLine($"El numero mayor es {Numero2}");
+            }
+
+            else
+
+                Console.WriteLine($"El numero mayor es {Numero3}");
+
+            int hora = 0, minutos =0, segundos =0;
+
+            Console.WriteLine("Escribe la hora");
+            hora = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe la hora");
+            minutos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe la hora");
+            segundos = Convert.ToInt32(Console.ReadLine());
+
+            if (hora >= 0 && hora <= 23 && minutos >= 0 && minutos <= 59 && segundos >= 0 && segundos <= 59)
+            {
+                Console.WriteLine($"La hora introducida es:{hora:D2} {minutos:D2}{segundos:D2} ");
+            }
+            else
+            {
+                Console.WriteLine("El formato no es el adecuado");
+            }
+
+
+        }
+    }
+}
