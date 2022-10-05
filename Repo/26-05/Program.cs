7bcf98f7d47406030a71c7d03660bd3cef586559
Vmiguel96 <103062540+Vmiguel96@users.noreply.github.com>
2022-10-05 18:03:42 +0200

    Add files via upload

---
+++ Repo/26-05/Program.cs
@@ -0,0 +1,385 @@
+﻿using System;
+
+namespace _26_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Juego Suma Numero randow
+
+            //int numero, resultado = 0,numerUsuario;
+            //string nombre,seguir="S";
+
+            //int contador = 0;
+            //Random r = new Random();
+            //Console.Write("Nombre: ");
+            //nombre = Console.ReadLine();
+
+
+
+            //while (seguir == "S")
+            //{
+            //    contador = 0;
+            //    resultado = 0;
+            //    do
+            //    {
+            //        numero = r.Next(1, 51);
+            //        Console.WriteLine($"Numero: {numero}");
+            //        numerUsuario = Convert.ToInt32(Console.ReadLine());
+            //        resultado += numero;
+            //        if (numerUsuario == resultado)
+            //        {
+            //            contador++;
+            //        }
+            //    } while (numerUsuario == resultado);
+            //    Console.WriteLine($"{nombre} acertaste {contador - 1} veces");
+            //    Console.WriteLine($"Quieres volver a jugar {nombre} (S/N)");
+            //    seguir = Console.ReadLine();
+            //}
+
+            // Menu Repetitivo
+
+            //double numero1, numero2;
+            //string opcion;
+
+            //do
+            //{
+
+            //Console.Write("\nEscribe un numero: ");
+            //numero1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("\nEscribe un numero: ");
+            //numero2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            //Console.WriteLine("\t 1 Sumar");
+            //Console.WriteLine("\t 2 Restar ");
+            //Console.WriteLine("\t 3 Multiplicar");
+            //Console.WriteLine("\t 4 Dividir");
+            //Console.WriteLine("\t 5 Salir del Programa");
+
+            //Console.WriteLine("Selecione opción");
+            //opcion = Console.ReadLine();
+
+            //    switch (opcion)
+
+            //    {
+            //        case "1":
+            //            Console.Write($"Suma de {numero1} + {numero2} es {numero1 + numero2}");
+            //            break;
+            //        case "2":
+            //            Console.Write($"Resta de {numero1} - {numero2} es {numero1 - numero2}");
+            //            break;
+            //        case "3":
+            //            Console.Write($"Multiplicacion de {numero1} * {numero2} es {numero1 * numero2}");
+            //            break;
+            //        case "4":
+            //            if (numero2 != 0)
+            //            {
+            //                Console.Write($"Division de {numero1} / {numero2} es {numero1 / numero2}");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine($"No se puede dividir por {numero2}");
+            //            }
+            //            break;
+            //        case "5":
+
+            //            break;
+
+
+            //    }
+            //} while (opcion != "5");
+
+            //Ejercicio For
+            //int num;
+            //Console.WriteLine("Introduce el numero a multiplicar: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"Tabla del numero {num}*{i}={num*i}");
+            //}
+
+            //Ejercicio 9 Contar Pares
+
+            //int numeroUsuario,pares=0,impares=0;
+
+            //Console.WriteLine("Introduce un numero");
+            //numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= numeroUsuario; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        pares++;
+            //    }
+            //    else
+            //    {
+            //        impares++;
+            //    }
+            //}
+            //Console.WriteLine($"Hay {pares} números pares");
+            //for (int i = 2; i <= numeroUsuario; i=+2)
+            //{
+            //        pares++;
+            //}
+            //Console.WriteLine($"Hay {pares} números pares");
+
+            // Ejercicio 10 Contar Pares e impares
+
+            //Console.WriteLine("Introduce un numero");
+            //numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= numeroUsuario; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        pares++;
+            //    }
+            //    else
+            //    {
+            //        impares++;
+            //    }
+            //}
+            //Console.WriteLine($"Hay {pares} números pares , hay {impares} numeros impares");
+
+            //Ejercicio 11
+
+            //int numeroCuadrado;
+
+            //Console.WriteLine("Introduce un numero");
+            //numeroCuadrado = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i < numeroCuadrado; i++)
+            //{
+            //    Console.WriteLine($"{Math.Pow(i,2)}");
+            //}
+
+            //Ejercicio 12
+
+            //int numeroDivisible;
+            //Console.WriteLine("Introduce un numero");
+            //numeroDivisible = Convert.ToInt32(Console.ReadLine());
+            //bool numeroPrimo = true;
+
+            //for (int i =2; i<numeroDivisible-1;i++) 
+            //{
+            //    if (numeroDivisible%i==0)
+            //    {
+            //        numeroPrimo = false;
+            //    }
+            //}
+            //if (numeroPrimo)
+            //{
+            //    Console.WriteLine($"El numero {numeroDivisible} es  Primo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"El numero {numeroDivisible} no es  Primo");
+            //}
+
+            //Ejercicio 1
+
+            //for (int i = 20; i >= 1; i--)
+            //{
+            //    Console.WriteLine($" {i}");
+            //}
+
+            //Ejercio 2
+
+            //int suma,variableUsuario = 0;
+
+            //Console.WriteLine("Introduce un numero");
+            //variableUsuario = Convert.ToInt32(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Introduce un numero");
+            //    variableUsuario = Convert.ToInt32(Console.ReadLine());
+            //    suma = suma + variableUsuario;
+            //    Console.WriteLine($"Valor de la Variable acumulado es: {variable}");
+            //} while (variableUsuario !=0);
+
+
+            //for (int i=0; i <= 0; i--) 
+            //{
+            //    Console.WriteLine("Introduce un numero");
+            //    variableUsuario = Convert.ToInt32(Console.ReadLine());
+            //    suma = suma + variableUsuario;
+            //    Console.WriteLine($"Valor de la Variable acumulado es: {variable}");
+            //    if (variableUsuario==0) 
+            //    {
+            //        i = 23;
+            //      //break Rompe el Bucle
+            //    }
+            //}
+
+            // Ejercicio 3
+
+            //int numero =0 ;
+            //int cifras = 0;
+
+            //    Console.WriteLine("Introduce un numero");
+            //    numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i <= 0; i--)
+            //{
+            //    numero = numero / 10;
+            //    cifras++;
+            //    if (numero == 0)
+            //    {
+            //        i = 23;
+            //    }
+            //}
+            //Console.WriteLine($" Hay {cifras} cifras ");
+
+            //Ejercicio 4
+
+            //int numero = 0;
+
+
+            //Console.WriteLine("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int  i=1; i<=numero;i++) 
+            //{
+            //    Console.Write("*");
+            //}
+
+            //for (int i = 0; i <= 0; i--)
+            //{
+            //    numero = numero / 10;
+            //    Console.Write("*");
+            //    if (numero == 0)
+            //    {
+            //        i = 23;
+            //    }
+            //}
+
+            //Ejercicio 5
+
+            //for (int i=1;i<=30;i++) 
+            //{
+            //    if (i %3==0)
+            //    {
+            //        Console.WriteLine($"{i}");
+            //    }
+            //}
+
+            ////Ejercicio 6
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine($"{i}");
+            //    }
+            //}
+            ////Version sin If
+            //for (int i = 1; i <= 100; i=i+5)
+            //{
+            //        Console.WriteLine($"{i}");   
+            //}
+
+            //Ejercicio 7
+            //int numero;
+            //Console.WriteLine("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i =1; i<=numero;i++)
+            //{
+            //    if (i != numero )
+            //    {
+            //        Console.Write($"{i},");
+            //    }
+            //    else 
+            //    {
+            //        Console.Write($"{i}");
+            //    }
+            //}
+
+            //int numero;
+            //Console.WriteLine("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Los divisores de {numero} son: ");
+            //for (int i=1; i<=numero;i++) 
+            //{
+            //    if (numero%i==0) 
+            //    {
+            //        Console.Write($" {i} ");
+            //    }
+            //}
+
+            //Ejercicio 10
+            //for(int i=1; i<=5; i++)
+            //{
+            //    for (int j=1;j<=9;j++) 
+            //    {
+            //        Console.Write($"{j}");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            //Ejercicio 11
+            //for (int i = 9; i >= 1; i--)
+            //{
+
+            //        for (int j = 1; j<= i; j++)
+            //        {
+            //            Console.Write($"{j}");
+            //        }
+            //        Console.Write("\n");
+
+            //}
+
+
+            //Ejercicio 12
+            int numero;
+            Console.Write("Introduce numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int filas=1; filas<=numero ; filas++)
+            //{
+            //    for (int columnas=1 ; columnas<=numero ; columnas++) 
+            //    {
+            //        Console.Write($"*");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            //Ejercicio 13
+            //for (int filas = 1; filas <= numero; filas++)
+            //{
+            //    for (int columnas = 1; columnas <= filas; columnas++)
+            //    {
+            //        Console.Write($"*");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            //Ejercicios 14
+
+            for (int filas = 1; filas <= numero; filas++)
+            {
+                for (int columnas = 1; columnas <= numero; columnas++)
+                {
+                    if (columnas <= numero - filas)
+                    {
+                        Console.Write($"");
+                    }
+                    else
+                    {
+                        Console.Write($"*");
+                    }
+                }
+                Console.Write("\n");
+
+                //Ejercicio 15
+            }
+        }
+    }
+}
---
+++ Repo/27-05/Program.cs
@@ -0,0 +1,360 @@
+﻿using System;
+
+
+namespace _27_05
+{
+    class Program
+    {
+        public static void Ejercicio1()
+        {
+            string[] meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre" };
+            Console.WriteLine("Introduce un numero correspondiente al mes");
+            int numero = 0;
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El Mes elegido es {meses[numero-1]}");
+        }
+
+        public static void Ejercicio2()
+        {
+             int[] numeros = new int[10];
+            int suma = 0;
+            
+
+            for (int i =0; i<numeros.Length;i++)
+            {
+                Console.Write($"Introduce el numero {i+1}: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                suma = suma + numeros[i];
+                Console.Write($"{numeros[i]},");
+            }
+            Console.WriteLine($"\nSuma de los valores es: {suma}");
+
+            double media = suma / (double)numeros.Length;
+
+            Console.WriteLine($"La Media de los Valores es: {media}");
+
+            int valorMax = numeros[0],valorMin = numeros [0];
+
+            for (int i = 1;i <numeros.Length; i++) 
+            {
+                if (numeros[i] > valorMax) 
+                {
+                    valorMax = numeros[i];
+                }
+                if (numeros[i]< valorMin) 
+                {
+                    valorMin = numeros[i];
+                }
+                
+            }Console.WriteLine($"Numero mayor es: {valorMax} \nNumero Mayor es: {valorMin}");
+        }
+
+        public static void Ejercicio3() 
+        {
+            double[] numeros = new double [10];
+            
+            
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Introduce el numero {i + 1}: ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double suma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                suma += numeros[i];
+                Console.Write($"{numeros[i]},");
+            }
+
+            double media = suma / numeros.Length;
+
+            Console.WriteLine($"La Media de los Valores es: {media:F2}");
+            Console.WriteLine($"Valores por encima de la media");
+
+            for (int i=1; i<numeros.Length;i++)
+            {
+                if (numeros[i] >media)
+                {
+                    Console.Write($"{numeros[i]} ");
+                }
+            }
+        }
+
+        public static void Ejercicio4()
+        {
+            int[] dias = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"  };
+
+            int mes = 0, dia = 0;
+            int totalDias = 0;
+            string mesLetra = "";
+            bool encontrado = false;
+            int posicionMes = 0;
+            int mesEncontrado = 0;
+
+            Console.WriteLine("Introduce un mes");
+            //mes = Convert.ToInt32(Console.ReadLine());
+            mesLetra = Console.ReadLine();
+
+            Console.WriteLine("Introduce un dia");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            //Buscamos el mes introducido
+            
+
+            for (posicionMes= 0; posicionMes<meses.Length && !encontrado;posicionMes++) 
+            {
+                if (meses[posicionMes] == mesLetra) 
+                {
+                    encontrado = true;
+                    mesEncontrado = posicionMes;
+                }
+            }
+
+            for (int i =0;i<mesEncontrado;i++)
+            {
+                    totalDias += dias[i];   
+            }
+            totalDias = totalDias + dia;
+            Console.WriteLine($"El {dia} de {meses[mesEncontrado]} es el dia {totalDias} del año  ");
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Ejercicio 13
+            //int numeroDivisible;
+            //Console.WriteLine("Introduce un numero");
+            //numeroDivisible = Convert.ToInt32(Console.ReadLine());
+            //bool numeroPrimo = true;
+
+            //for (int i = 1;i<=numeroDivisible;i++ )
+            //{
+            //    numeroPrimo = true;
+            //    for (int j = 2; j <= i - 1; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            numeroPrimo = false;
+            //        }
+            //    }
+            //    if (numeroPrimo)
+            //    {
+            //            Console.Write($"{i},");
+            //    }
+            //}
+
+            //Ejercicio 14
+            //int numero = 0;
+            //Console.WriteLine("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i =numero; i>=1;i--)
+            //{
+            //    for (int j=0; j <i; j++)
+            //    {
+            //    Console.Write($"#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Ejercicio 15
+
+            //int numero = 0;
+            //Console.WriteLine("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i =1; i< numero; i++)
+            //{
+            //    for (int j = 1; j <= numero; j++)
+            //    {
+
+            //        if (j<i)
+            //        {
+            //            Console.Write($" ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write($"#");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //int numero = 0;
+            //Console.Write("Introduce un numero");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < numero; i++)
+            //{
+            //        for (int espacios=0;espacios<i;espacios++)
+            //        {
+            //            Console.Write($" ");
+            //        }
+            //        for(int caracteres=0;caracteres<numero-i;caracteres++)
+            //        {
+            //            Console.Write($"#");
+            //        }
+            //    Console.WriteLine();
+            //}
+
+            //Ejercicio 16
+            //int numero = 0;
+            //Console.Write("Introduce un numero ");
+            //numero = Convert.ToInt32(Console.ReadLine());
+            //int factorial = 1;
+
+            //for (int i =numero ; i > 0; i--)
+            //{ 
+            //    {
+            //        factorial *= i;
+            //    }
+            //}
+            //Console.WriteLine($"Factorial de {numero} es {factorial}");
+
+            //Ejercicio 20 y 21
+            //int numero = 1;
+
+            //int contador = 0;
+
+            //while (numero != 0) 
+            //{
+            //    Console.Write("Introduce un numero ");
+            //    numero = Convert.ToInt32(Console.ReadLine());
+            //    if (numero %5==0 && numero !=0) 
+            //    {
+            //        contador++;
+            //    }
+            //}
+            //Console.WriteLine($"Numeros introducidos {contador} multiplos de 5");
+
+            //Ejercicio 17
+            //int cantidad, precio;
+            //int total=0,sumaTotal= 0;
+
+            //Console.WriteLine("Introduce cantidad: ");
+            //cantidad = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Introduce precio: ");
+            //precio = Convert.ToInt32(Console.ReadLine());
+
+            //while (!(cantidad ==0 || precio==0)) 
+            //{
+            //    total = cantidad * precio;
+            //    sumaTotal += total;
+            //    Console.WriteLine($"Total: {total}");
+
+            //    Console.WriteLine("Introduce cantidad: ");
+            //    cantidad = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Introduce precio: ");
+            //    precio = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine($"Total Final: {sumaTotal}");
+
+            //Ejercicio 18
+            //int numero1,numero2;
+            //int contador = 0;
+
+            //Console.WriteLine("Introduce un numero: ");
+            //numero1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Introduce otro numero: ");
+            //numero2 = Convert.ToInt32(Console.ReadLine());
+
+            //bool numeroPrimo = true;
+
+            //for (int i = numero1; i <= numero2; i++)
+            //{
+            //    numeroPrimo = true;
+            //    for (int j = 2; j <= i - 1; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            numeroPrimo = false;
+            //        }
+            //    }
+            //    if (numeroPrimo)
+            //    {
+            //        contador++;
+            //    }
+            //}
+            //Console.WriteLine($"Hay {contador} numeros primos");
+
+            ////Ejercio 19
+
+            //int numero1, numero2,numero;
+            //Console.WriteLine("Introduce un numero: ");
+            //numero1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Introduce otro numero: ");
+            //numero2 = Convert.ToInt32(Console.ReadLine());
+
+
+            //if (numero1 > numero2)
+            //{
+            //    numero = numero2;
+            //}
+            //else 
+            //{
+            //    numero = numero1;
+            //}
+
+            //for (int i =1;i<=numero;i++ ) 
+            //{
+
+            //        if (numero1 % i == 0 && numero2%i==0)
+            //        {
+            //            Console.WriteLine(i+" ");
+            //        }
+            //}
+
+
+            //int[] numeros = new int[5];
+
+            //numeros[0] = 12;
+            //numeros[1] = 23;
+            //numeros[2] = 53;
+            //numeros[3] = 5;
+            //numeros[4] = 92;
+
+            //int[] numeros2 = { 12, 23, 53, 5, 92 };
+            //Console.WriteLine($"Lenght = {numeros2.Length}");
+            ////Recorrer array
+            //for (int i=0; i<numeros2.Length;i++)
+            //{
+            //    Console.WriteLine($"En la posicion {i} esta el valor {numeros2[i]}");
+            //}
+            ////foreach
+            //foreach (int num in numeros)
+            //{
+            //    Console.WriteLine($"el valor {num}");
+            //}
+
+
+            ////Pedir Numero 
+            //for (int i = 0;i < 100;i++)
+            //{
+            //    if (100 % i == 0)
+            //    {
+
+            //    }
+            //}
+
+            //Ejercicio1();
+            //Ejercicio2();
+            //Ejercicio3();
+            Ejercicio4();
+        }
+    }
+}
