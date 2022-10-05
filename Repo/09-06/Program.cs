6d37f0f6536ec3151c497b5c43772cf0478a1c00
Vmiguel96 <103062540+Vmiguel96@users.noreply.github.com>
2022-10-05 17:59:44 +0200

    Add files via upload

---
+++ Repo/09-06/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+
+namespace _09_06
+{
+    class Program
+    {
+        public static void Espacios()
+        {
+            Console.WriteLine("Escribre una frase");
+            string palabra = Console.ReadLine();
+            string palabraEspacio = "";
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (i < palabra.Length)
+                {
+                    palabraEspacio += palabra[i] + " ";
+                }
+                else
+                {
+                    palabraEspacio += palabra[i];
+                }
+
+            }
+
+
+
+
+            //Opcion1
+            //for (int i = 0; i < palabra.Length; i++)
+            //{
+            //        palabraEspacio += palabra[i] + " ";
+            //    if (i < palabra.Length)
+            //        palabraEspacio += palabra[i];
+            //}
+            //Console.WriteLine($"{palabraEspacio}");
+            //Opcion2
+            //for (int i = 0; i < palabra.Length; i++)
+            //{
+            //        palabraEspacio += palabra[i] + " ";
+            //}
+            //        palabraEspacio += palabra[i];
+            //Console.WriteLine($"{palabraEspacio}");
+
+        }
+
+        public static void Cadenas()
+        {
+            Console.WriteLine("Escribe una frase");
+            string cadena = Console.ReadLine();
+
+            string mayuscula = cadena.ToUpper();
+            string minuscula = cadena.ToLower();
+            string borrar = cadena.Remove(1, 2);
+            string insertar = cadena.Insert(2, "yo");
+            string remplazar = cadena.Replace(" ", "_");
+            string quitarEspaciosIzq = cadena.TrimStart();
+            string quitarEspaciosDer = cadena.TrimEnd();
+            string remplazarA = cadena.Replace("a", "A");
+            string[] trocear = cadena.Split();
+
+            for (int i = 0; i < trocear.Length; i++)
+            {
+                Console.Write(trocear[i]);
+            }
+
+            Console.WriteLine($"Mayusculas: {mayuscula}");
+            Console.WriteLine($"Minusculas: {minuscula}");
+            Console.WriteLine($"Borrar segunda y tercera letra: {borrar}");
+            Console.WriteLine($"Insertar yo en la segunda y tercera linea: {insertar}");
+            Console.WriteLine($"Remplazar espacios por _: {remplazar}");
+            Console.WriteLine($"Quitar espacios a la izquierda: {quitarEspaciosIzq}");
+            Console.WriteLine($"Quitar espacios a derecha: {quitarEspaciosDer}");
+            Console.WriteLine($"Remplazar a minuscula por mayuscula: {remplazarA}");
+            //Console.WriteLine($"Contains: {cadena.Contains("la")?$"{cadena} contiene");
+
+
+
+        }
+
+        public static string PonerTexto(string frase) 
+        {
+            int mayusculas = 0;
+            for (int i=0;i<frase.Length;i++) 
+            {
+                if (frase[i]>='A' && frase[i]<='Z' || frase[i]== 'Ñ') 
+                {
+                    mayusculas++;
+                }
+            }
+            if (mayusculas==frase.Length) 
+            {
+               frase = frase.ToLower();
+               frase = frase.Substring(0, 1).ToUpper() + frase.Substring(1);
+            }
+
+            return frase;
+
+        }
+
+        public static void GestionArrays()
+        {
+            string opcion = "";
+            string[] nombres = new string[10];
+            bool Salir = false;
+            do {
+
+                Console.WriteLine("1. Rellenar Array");
+                Console.WriteLine("2. Buscar un nombre concreto");
+                Console.WriteLine("3. Buscar nombres que empiezen por: ");
+                Console.WriteLine("4. Buscar un nombre que contenga un texto introducido");
+                Console.WriteLine("5. Salir");
+                Console.WriteLine("Introduce un opción");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+
+                    case "1":
+                        RellenarArray(nombres);
+                        break;
+                    case "2":
+                        BuscarNombre(nombres);
+                        break;
+                    case "3":
+                        BuscarPosteriores(nombres);
+                        break;
+                    case "4":
+                        BuscarContenga(nombres);
+                        break;
+                    case "5":
+                        Salir = true;
+                        break;
+                }
+            }while (!Salir) ;
+
+        }
+
+
+        public static void RellenarArray(string[] nombres) 
+        {
+            for (int i=0;i<10;i++) 
+            {
+                Console.WriteLine($"Introduce nombre: {i+1}");
+                nombres[i] = Console.ReadLine();
+            }
+            
+        }
+
+        public static void BuscarNombre(string[] nombres) 
+        {
+            string nombre = "";
+            Console.WriteLine("Introduce un nombre a buscar");
+            nombre = Console.ReadLine();
+
+            for (int i=0;i<nombres.Length;i++) 
+            {
+                if (nombres[i] == nombre)
+                {
+                    Console.WriteLine($"{i+1}. {nombres[i]}");
+                }
+            }
+        }
+
+
+
+
+        public static void BuscarPosteriores(string[] nombres) 
+        {
+
+            Console.WriteLine("Introduce texto");
+            string texto = Console.ReadLine();
+            for (int i=0;i<nombres.Length;i++) 
+            {
+                if (String.Compare(nombres[i],texto)>0) 
+                {
+                    Console.WriteLine($"{i+1}. {nombres[i]}");
+                }
+            }
+        }
+
+        public static void BuscarContenga(string[] nombres)
+        {
+
+            Console.WriteLine("Introduce texto");
+            string texto = Console.ReadLine();
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                if (nombres[i].Contains(texto))
+                {
+                    Console.WriteLine($"{i + 1}. {nombres[i]}");
+                }
+            }
+        }
+
+
+        public static void NombreApellido(string nombreApellido) 
+        {
+            Console.WriteLine("Introduzca Nombre y Apellido");
+            nombreApellido = Console.ReadLine();
+            nombreApellido = nombreApellido.Trim();
+            string cadenaRemplazo = "";
+            string[] trozos = nombreApellido.Split(' ');
+
+            if (trozos.Length >= 3)
+            {
+                cadenaRemplazo = "Sr/Sra" + trozos[2] + "," + trozos[0] + " " + trozos[1];
+            }
+            else
+            {
+                //Console.WriteLine(String.Join(" ,", trozos));
+                cadenaRemplazo = "Sr/Sra" + trozos[1] + "," + trozos[0];
+                Console.WriteLine(cadenaRemplazo);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Espacios();
+            //Cadenas();
+            //GestionArrays();
+            Console.WriteLine(PonerTexto("HOLAQUETAL"));
+            NombreApellido("");
+        }
+    }
+}
---
+++ Repo/11-06/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.IO;
+
+namespace _11_06
+{
+    class Program
+    {
+        public static void Ejercicio1() //Leer ficheros
+        {
+            int suma = 0;
+            string linea;
+            int contarLineas = 0;
+
+
+            //Opcion 1
+            StreamReader lector = new StreamReader("numeros.txt"); //Declaramos variables tipo StreamReaader
+            while ((linea = lector.ReadLine()) != null) //Recorremos el array hasta encontrar null
+            {
+                contarLineas++;
+            }
+
+            lector.Close();
+
+            int[] numeros = new int[contarLineas];
+            contarLineas = 0;
+
+            lector = new StreamReader("numeros.txt");
+
+            while ((linea = lector.ReadLine()) != null) //Recorremos el array hasta encontrar null
+            {
+                suma += Convert.ToInt32(linea);
+                numeros[contarLineas] = Convert.ToInt32(linea);
+                contarLineas++;
+            }
+
+            
+            Console.WriteLine($"{String.Join("+",numeros)}={suma}");
+
+            lector.Close();//SIEMPRE HE DE CERRAR
+            //Opcion 2
+            suma = 0;
+            string[] lineas = File.ReadAllLines("numeros.txt");
+            foreach (string l in lineas) 
+            {
+                suma += Convert.ToInt32(l);
+            }
+            Console.WriteLine($"{String.Join("+", numeros)}={suma}");
+        }
+
+        public static void Ejercicio2() 
+        {
+            string texto = "";
+            StreamWriter writer = new StreamWriter("Ejercicio2.txt");
+
+            while (texto !="FIN") {
+
+                Console.WriteLine("Escribe las lineas hasta que escribas (fin)");
+                texto = Console.ReadLine();
+                writer.WriteLine(texto);
+                
+                
+            }
+            writer.Close();
+
+            
+        }
+
+        public static void Ejercicio26() 
+        {
+            string pelicula="";
+            Console.WriteLine("Introduce una lista de peliculas separadas por un -");
+            pelicula = Console.ReadLine();
+            string[] peliculas = pelicula.Split("-");
+            
+        }
+
+        public static bool ComprobarCaractertes(char c)
+        {
+            bool correcto= true;
+            string especiales = "!#$%&'*+-/=?^_`{|}~;";
+            
+                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+                {
+                    if (!(c >= '0' && c <= '9'))
+                    {
+                        if (!(especiales.IndexOf(c)>=0))
+                        {
+                            correcto=false;
+                         }
+                    }
+                }
+                
+
+
+
+
+
+            
+            return correcto;
+        }
+
+
+
+        //public static bool ComprobarEmail(string cadena) 
+        //{
+        //    bool correcto true;
+        //    for (int i=0;i<cadena.Length;i++)
+        //    {
+        //        if () 
+        //        {
+
+        //        }
+        //    }
+        //}
+
+        static void Main(string[] args)
+        {
+            string cadena="sdsa@df.";
+            bool corecto = true;
+            for (int i=0;i<cadena.Length;i++) 
+            {
+                if (!(cadena[i]=='@') && !(cadena[i]=='.')) 
+                {
+                    if (!ComprobarCaractertes(cadena[i])) 
+                    {
+                        corecto = false;
+                    }
+                }
+            }
+            if (cadena[0]=='.' || cadena[cadena.Length-1]=='@') 
+            {
+                corecto = false;
+            }
+            if (cadena.IndexOf('@') >= 0)
+            {
+                if (!(cadena.IndexOf('@') == cadena.LastIndexOf('@')))
+                {
+                    corecto = false;
+                }
+            }
+            else 
+            {
+                corecto = false;
+            }
+            if (cadena.IndexOf('.') >= 0)
+            {
+                if (!(cadena.IndexOf('.') == cadena.LastIndexOf('.')))
+                {
+                    corecto = false;
+                }
+            }
+            else 
+            {
+                corecto = false;
+            }
+
+            Console.WriteLine(corecto? "Email  valido": "Email no valido");
+            //string temporal = "";
+            //string cadena = "";
+            //string[] palabras;
+            //Console.WriteLine("Introduce una lista de peliculas separadas por un -");
+            //cadena = Console.ReadLine();
+            //string[] peliculas = cadena.Split('-');
+
+            //for (int i=0;i<peliculas.Length;i++) 
+            //{
+            //    palabras = peliculas[i].Split();
+            //    if (palabras[0].ToUpper() == "LA" || palabras[0].ToUpper() == "EL" || palabras[0].ToUpper() == "LOS" || palabras[0].ToUpper() == "LAS")
+            //    {
+                    
+            //        for (int j=1;j<palabras.Length;j++) 
+            //        {
+            //            temporal += palabras[j]+" ";
+                            
+            //        }
+
+            //        temporal = temporal.TrimEnd();
+            //        temporal += ", " + palabras[0];
+            //        peliculas[i] = temporal;
+            //    }
+            //    peliculas[i]=peliculas[i].ToUpper();
+
+            //}
+
+            //Array.Sort(peliculas);
+
+            //for (int i=0;i<peliculas.Length;i++) 
+            //{
+            //    Console.WriteLine($"{i+1}. {peliculas[i]}");
+            //}
+
+
+            //int contador = 0;
+            //string line; System.IO.StreamReader file = new System.IO.StreamReader("prueba.txt"); //Declaramos variables tipo StreamReaader
+            //while ((line = file.ReadLine()) != null) //Recorremos el array hasta encontrar null
+            //{
+            //    System.Console.WriteLine(line);
+            //    contador++;
+            //}
+            //file.Close();//SIEMPRE HE DE CERRAR
+            //Console.WriteLine(contador);
+            //Console.WriteLine("");
+            //string texto = File.ReadAllText("prueba.txt");
+            //Console.WriteLine(texto);
+
+
+            //string[] l2 = texto.Split("\n");
+            //Console.WriteLine(l2.Length);
+
+            //foreach (string l in l2)
+            //{
+            //    Console.WriteLine(l);
+            //}
+            //Ejercicio1();
+            //Ejercicio2();
+
+        }
+    }
+}
